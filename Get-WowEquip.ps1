6f476873f49c808763e5d71c22d804b26f03a514
Loats <runfar@pm.me>
2020-04-19 16:43:27 -0500

    Pets and Equipment!

---
+++ Get-WowEquip.ps1
@@ -0,0 +1,28 @@
+$Creds = Import-Csv ..\creds.csv
+$ClientSecret = $Creds.clientSecret
+$ClientID = $Creds.clientID
+
+$char = Read-Host -Prompt "Character Name?"
+$realm = Read-Host -Prompt "Realm?"
+
+$Url = "https://us.battle.net/oauth/token"
+
+$Body = @{
+  'client_id' = $ClientID
+  'client_secret' = $ClientSecret
+  'grant_type' = 'client_credentials'
+}
+
+$token = Invoke-RestMethod -Method Post -Uri $Url -Body $Body
+
+# $baseuri = /profile/wow/character/{realmSlug}/{characterName}/equipment
+
+$URL = "https://us.api.blizzard.com/profile/wow/character/" + $realm + "/" + $char +  "/equipment?namespace=profile-us&locale=en_US&access_token=" + $token.access_token
+
+#$url
+
+$equip = Invoke-WebRequest -Uri $URL
+
+$equipxml = $equip | ConvertFrom-Json
+
+$equipxml.equipped_items | ForEach-Object {Write-Host $_.quality.name, $_.slot.name, $_.name, $_.level.value}
---
+++ Get-Wowpets.ps1
@@ -0,0 +1,28 @@
+$Creds = Import-Csv ..\creds.csv
+$ClientSecret = $Creds.clientSecret
+$ClientID = $Creds.clientID
+
+$char = Read-Host -Prompt "Character Name?"
+$realm = Read-Host -Prompt "Realm?"
+
+$Url = "https://us.battle.net/oauth/token"
+
+$Body = @{
+  'client_id' = $ClientID
+  'client_secret' = $ClientSecret
+  'grant_type' = 'client_credentials'
+}
+
+$token = Invoke-RestMethod -Method Post -Uri $Url -Body $Body
+
+# $baseuri = /profile/wow/character/{realmSlug}/{characterName}/equipment
+
+$URL = "https://us.api.blizzard.com/profile/wow/character/" + $realm + "/" + $char +  "/collections/pets?namespace=profile-us&locale=en_US&access_token=" + $token.access_token
+
+#$url
+
+$pets = Invoke-WebRequest -Uri $URL
+
+$petsxml = $pets | ConvertFrom-Json
+
+$petsxml.pets | ForEach-Object {Write-Host $_.quality.type, $_.level, $_.species.name}
